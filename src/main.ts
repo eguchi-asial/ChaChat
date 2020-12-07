c73a61ee082952586a3ff5b8e5d850330985b881
eguchi <eguchi@asial.co.jp>
2020-12-07 09:34:08 +0900

    GoogleFireBase導入

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,25 @@
-import { createApp } from 'vue';
+import { createApp, provide } from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+/* import Firebase */
+import firebase from 'firebase';
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.VUE_APP_GFB_APIKEY,
+  authDomain: process.env.VUE_APP_GFB_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_GFB_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_GFB_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_GFB_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_GFB_APP_ID,
+  measurementId: process.env.VUE_APP_GFB_MEASUREMENT_ID
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 createApp(App)
   .use(store)
